d4973978d93dad12dea3620d32a6b0c0a7877f5a
Takara Truong <truont2@gmail.com>
2022-09-28 15:33:19 -0700

    insert position

--- 092822.js
+++ 092822.js
@@ -10,26 +10,26 @@ The tests are generated such that there is exactly one solution. You may not use
 Your solution must use only constant extra space.
 */
 
-let numbers = [0,0,11,15], target = 0;
+// let numbers = [0,0,11,15], target = 0;
 
-var twoSum = function(numbers, target) {
-    let right = numbers.length - 1;
-    let left = 0;
+// var twoSum = function(numbers, target) {
+//     let right = numbers.length - 1;
+//     let left = 0;
 
-    while(left < right) {
-        let sum = numbers[left] + numbers[right];
+//     while(left < right) {
+//         let sum = numbers[left] + numbers[right];
 
-        if(sum === target) {
-            return [left + 1, right + 1]
-        } else if(sum < target) {
-            left++;
-        } else {
-            right--;
-        }
-    }
+//         if(sum === target) {
+//             return [left + 1, right + 1]
+//         } else if(sum < target) {
+//             left++;
+//         } else {
+//             right--;
+//         }
+//     }
 
-    return null;
-};
+//     return null;
+// };
 // two pointers or double for loop
 // console.log(twoSum(numbers, target));
 
@@ -45,30 +45,91 @@ All occurrences of a character must be replaced with another character while pre
 
 // Input: s = "egg", t = "add" => output true
 
-var isIsomorphic = function(s, t) {
-    // store everything into two map
+// var isIsomorphic = function(s, t) {
+//     // store everything into two map
 
-    let charMapS ={};
-    let charMapT = {}
-    for(let i = 0; i < s.length; i++) {
-        // if seeing for the first time
-        if(charMapS[s.charAt(i)] === undefined && charMapT[t.charAt(i)] === undefined) {
-            charMapS[s.charAt(i)] = t.charAt(i);
-            charMapT[t.charAt(i)] = s.charAt(i)
-        } 
+//     let charMapS ={};
+//     let charMapT = {}
+//     for(let i = 0; i < s.length; i++) {
+//         // if seeing for the first time
+//         if(charMapS[s.charAt(i)] === undefined && charMapT[t.charAt(i)] === undefined) {
+//             charMapS[s.charAt(i)] = t.charAt(i);
+//             charMapT[t.charAt(i)] = s.charAt(i)
+//         } 
 
-        // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
-        // it doesn't match in either of the dictionaries or both 
-        if((charMapS[s.charAt(i)] !== t.charAt(i) && charMapT[t.charAt(i)] !== s.charAt(i))){
-            return false;
-        }
-    }
+//         // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
+//         // it doesn't match in either of the dictionaries or both 
+//         if((charMapS[s.charAt(i)] !== t.charAt(i) && charMapT[t.charAt(i)] !== s.charAt(i))){
+//             return false;
+//         }
+//     }
 
-    return true;
-};
+//     return true;
+// };
 
 // apporach: put words into separate maps
 // check if its the first time encountering the character anad save it
 // if it is already saved, make sure the mapping is mapping to the same character from both sides
-let s = "egg", t = "add"
-console.log(isIsomorphic(s, t));
+// let s = "egg", t = "add"
+// console.log(isIsomorphic(s, t));
+
+// reverse Linked list 
+
+// var reverseList = function(head) {
+//     let current = head;
+//     let nextNode = null;
+//     let previousNode = null;
+    
+//     while(current !== null) {
+//         next = current.next;
+//         current.next = previousNode;
+        
+//         previousNode = current;
+//         current = next;
+//     }
+    
+//     return previousNode;
+// };
+
+// search Insert Position
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity. */
+
+// must be binary search if they want it in O9Logn);;
+
+let nums = [1,3,5,6], target = 5;
+
+var searchInsert = function(nums, target) {
+    //     must be binary search so two pointers and mid needed
+    // move pinter based on value of target
+        let low = 0;
+        let high = nums.length - 1;
+        
+        while(low < high) {
+            let mid = Math.floor((low + high) / 2);
+    
+            if(target < nums[mid]) {
+                high = mid - 1;
+            } 
+            
+            if(target > nums[mid]){
+                low = mid + 1;
+            }
+        
+            
+            if(nums[mid] === target) {
+                return mid;
+            } 
+        }
+        
+    //     when low and mid land of the same spot
+    // check if its lower or higher index
+        if(target <= nums[low]) {
+            return low;
+        } else {
+            return low + 1;
+        }
+        
+        
+    };
