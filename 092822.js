ad863f138f1d0658ca1739aaa3307a0bd9a79e81
Takara Truong <truont2@gmail.com>
2022-09-28 18:16:48 -0700

    longest consecutive, review

--- 092822.js
+++ 092822.js
@@ -173,3 +173,10 @@ var searchInsert = function(nums, target) {
         return true;
     };
     
+
+// longest consecutive sequence
+/* 
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+*/
