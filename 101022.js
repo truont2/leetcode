809096a4f62f224350335b01e6c9d950bb9bf6bd
Takara Truong <truont2@gmail.com>
2022-10-10 14:29:22 -0700

    isAnagram

--- 101022.js
+++ 101022.js
@@ -1,5 +1,7 @@
 // valid parentheses
 
+const e = require("express");
+
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 // An input string is valid if:
 // Open brackets must be closed by the same type of brackets.
@@ -43,3 +45,37 @@ var isVlaid = function(s) {
 }
 
 console.log(isVlaid(example));
+
+// valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// input two strings
+// task: check if both of the strings contain the same characters
+// output: boolean
+
+var isAnagram = function(s, t) {
+    // can use two objects and can compare the count or add one string to an object, compare the keys and check if they exist
+
+    let sMap = {};
+
+    for(let char of s) {
+        if(sMap[char] === undefined) {
+            sMap[char] = 1;
+        } else {
+            sMap[char]++;
+        }
+    }
+
+    for(let i = 0; i < t.length; i++) {
+        let current = t.charAt(i);
+        if(sMap[current] !== undefined && sMap[current] > 0) {
+            sMap[current]--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
